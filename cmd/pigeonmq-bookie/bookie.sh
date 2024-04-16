4ce0003fd56500e2d40b2d4039eef600a9768e8f
Henry Hoo <976693458@qq.com>
2024-04-16 09:09:10 +0800

    Update: add notes

--- cmd/pigeonmq-bookie/bookie.sh
+++ cmd/pigeonmq-bookie/bookie.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # bookie.sh is a convenient tool to start/close a bookie process.
+#
+# Note: The current version of this script is used for testing. The
+# things to be done for production in the future are marked below.
+#
+# Author: Henry Hoo.
 
+
+# Check the input.
 if [ $# != 2 ];then
   echo "Usage: $0 <bookie_name> <option: start/close>"
   exit 1
 fi
 
+# TODO(Hoo@Future): Remove this variable in the future.
 VALID_BOOKIE_NAMES=("bookie_1" "bookie_2" "bookie_3")
 BOOKIE_NAME=$1
 
@@ -19,6 +27,7 @@ fi
 PID_FILE=/var/run/$BOOKIE_NAME.pid
 LOCK_FILE=/var/lock/$BOOKIE_NAME.lock
 
+# TODO(Hoo@Future): Modify the path.
 BIN=/home/hoo/Filebase/project/pigeonmq/cmd/pigeonmq-bookie/pigeonmq-bookie
 CONFIG=/home/hoo/Filebase/project/pigeonmq/configs/$BOOKIE_NAME.cfg
 
@@ -32,7 +41,6 @@ if [[ $OPTION == "start" ]]; then
   else
     echo "Error occurred in starting bookie $BOOKIE_NAME."
   fi
-
 elif [[ $OPTION == "close" ]]; then
   sudo kill -s SIGTERM $(cat $PID_FILE)
   if [[ $? == 0 ]]; then
